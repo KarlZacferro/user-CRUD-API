bde92823bf1e139ba750e20baf7bc701f3efe3fa
Karlisson Ferreira <kbb25t@gmail.com>
2025-03-06 23:31:19 -0300

    bug fix

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,12 +1,27 @@
-// backend/server.js
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Servidor rodando!');
-});
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
+// Conectar ao MongoDB
+mongoose.connect('mongodb+srv://Karl:kbbtyh2532@cluster0.tajs6.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Conectado ao MongoDB'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+
+// Rotas
+app.use('/users', userRoutes);
+
+// Iniciar o servidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
