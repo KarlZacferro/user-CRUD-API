3c1cb02710aee291a1cdd2f9d4ccbf4b3575c2e5
Karlisson <karlissonferreira42@gmail.com>
2026-06-05 18:59:04 -0300

    change URL

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
